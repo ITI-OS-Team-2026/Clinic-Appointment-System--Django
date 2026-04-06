71cc2467ad4afc872e0a947de6d5dfe338872eee
Ramadan-Elgamal <ramadanbotsher@gmail.com>
2026-04-06 13:55:53 +0200

    added the decorator for the autherization

--- users/views/auth_views.py
+++ users/views/auth_views.py
@@ -3,17 +3,36 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def login_view(request):
+    
+    # If they are already logged in, don't let them see the login page
+    if request.user.is_authenticated:
+        return redirect_based_on_role(request.user)
+    
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+        
         if user is not None:
             login(request, user)
-            return redirect('login')
+            return redirect_based_on_role(user)
         else:
             messages.error(request, 'Invalid username or password.')
     return render(request, 'auth/login.html')
 
+def redirect_based_on_role(user):
+    if user.role == 'PATIENT':
+        return redirect('patient_dashboard')
+    elif user.role == 'DOCTOR':
+        return redirect('doctor_queue')
+    elif user.role == 'RECEPTIONIST':
+        return redirect('receptionist_queue')
+    elif user.role == 'ADMIN':
+        return redirect('admin_dashboard')
+    else:
+        logout(user)
+        return redirect('login')
+    
 
 def logout_view(request):
     logout(request)
