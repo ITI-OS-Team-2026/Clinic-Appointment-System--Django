2b00fad8bfffe0e497974d032fe4a04a84175800
elsayedfarg <elsayedfarg792003@gmail.com>
2026-04-07 00:14:56 +0200

    Feat(Patient): more functions added to the serializer and small test for view

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PatientRegistrationSerializer
 
-# Create your views here.
+class PatientRegistrationView(CreateAPIView):
+    serializer_class = PatientRegistrationSerializer
+    # TODO
+    # Add permissions
+    
+    def create(self,req):
+        serializer=self.get_serializer(data=req.data)
+        
+        if serializer.is_valid():
+            patient=serializer.save()
+            
+            return Response(
+                {
+                    "message": "Patient registered successfully",
+                    "patient": PatientRegistrationSerializer(patient).data
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
