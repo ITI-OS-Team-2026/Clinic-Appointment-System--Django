f8a4b47db4e6c69ef9021858fa5c409d5f31d5ce
Ahmed Azab <ahmed-azab271>
2026-04-09 17:49:36 +0200

    Add URL routing for queue

--- records/urls.py
+++ records/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views.doctor_views import doctor_queue
-from .views.receptionist_views import receptionist_queue
+from .views.receptionist_views import receptionist_queue, check_in_patient
 
 urlpatterns = [
     path('doctor/queue/', doctor_queue, name='doctor_queue'),
     path('receptionist/queue/', receptionist_queue, name='receptionist_queue'),
+    path('receptionist/checkin/<int:appointment_id>/', check_in_patient, name='check_in_patient'),
 ]
