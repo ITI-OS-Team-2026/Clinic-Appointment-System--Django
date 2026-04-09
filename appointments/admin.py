47097280c119366de6e8de96738f5e982ba33363
Ahmed Azab <ahmed-azab271>
2026-04-09 17:49:53 +0200

    Register appointment-related models in the admin interface

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Appointment, AuditTrail, DoctorSchedule, ScheduleException
 
-# Register your models here.
+admin.site.register(Appointment)
+admin.site.register(AuditTrail)
+admin.site.register(DoctorSchedule)
+admin.site.register(ScheduleException)
