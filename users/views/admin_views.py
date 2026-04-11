26d08e7ddc15f19e5105a1a734049e67fd2f3a82
Ramadan-Elgamal <ramadanbotsher@gmail.com>
2026-04-11 17:41:17 +0200

    Add functionality to add a doctor in admin panel

--- users/views/admin_views.py
+++ users/views/admin_views.py
@@ -1,7 +1,41 @@
-from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
 from ..decorators import is_admin_role
+from users.models import User, DoctorProfile
 
+@login_required(login_url='login')
 @user_passes_test(is_admin_role)
-def admin_dashboard(request):
-    return HttpResponse("<h1>Admin Dashboard</h1><a href='/users/logout/'>Logout</a>")
+def add_doctor(request):
+    if request.method == 'POST':
+        u = request.POST.get('username')
+        p = request.POST.get('password')
+        e = request.POST.get('email')
+        
+        bio = request.POST.get('bio')
+        exp = request.POST.get('experience_years')
+        phone = request.POST.get('contact_number')
+        loc = request.POST.get('location')
+        spec = request.POST.get('specialization')
+        license_num = request.POST.get('license_number')
+
+        if User.objects.filter(username=u).exists():
+            messages.error(request, "That username is already taken.")
+            return redirect('add_doctor')
+
+        user = User.objects.create_user(username=u, email=e, password=p, role='DOCTOR')
+        
+        DoctorProfile.objects.create(
+            user=user, 
+            bio=bio, 
+            experience_years=exp, 
+            contact_number=phone,
+            location=loc,
+            specialization=spec,
+            license_number=license_num
+        )
+        
+        messages.success(request, f"Dr. {u} was successfully added to the clinic!")
+        return redirect('admin_dashboard')
+
+    return render(request, 'admin/add_doctor.html')
