1c6df0f8504c325803773c9c7d2483ae6aa31c8d
mohamedahmed-dev <mohamedahmed010320@gmail.com>
2026-04-13 01:13:47 +0200

    add consultation summary view for patient

---
+++ records/views/patient_views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import user_passes_test
+from appointments.models import Appointment
+from records.models import ConsultationRecord
+from users.decorators import is_patient
+
+from django.http import HttpResponseForbidden
+
+@user_passes_test(is_patient, login_url='/users/login/')
+def patient_consultation_summary(request, appointment_id):
+    appointment = get_object_or_404(Appointment, id=appointment_id)
+    
+    if appointment.patient != request.user:
+        return HttpResponseForbidden('Not allowed to view this consultation.')
+        
+    if appointment.status != 'COMPLETED':
+        return HttpResponseForbidden('Consultation is not completed.')
+        
+    consultation = get_object_or_404(ConsultationRecord, appointment=appointment)
+    
+    context = {
+        'appointment': appointment,
+        'consultation': consultation,
+        'prescriptions': consultation.prescriptions.all()
+    }
+    return render(request, 'patient/consultation.html', context)
