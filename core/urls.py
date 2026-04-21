59e69239b3d1d4bf9b5348c2ba89b1a9cddfa20a
mohamedahmed-dev <mohamedahmed010320@gmail.com>
2026-04-21 15:53:32 +0200

    feat: add landing page & unify navigation across app

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from users.views.auth_views import landing
 
 urlpatterns = [
+    path('', landing, name='landing'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('appointments/', include('appointments.urls')),
     path('records/', include('records.urls')),
-    path('', TemplateView.as_view(template_name="base.html"), name="home"),
+    path('home/', TemplateView.as_view(template_name="base.html"), name="home"),
     path("__reload__/", include("django_browser_reload.urls")),
      path('api/', include('availabilitySlots.urls')),
 ]
